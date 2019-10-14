cfbad8a9a5d54ca0b1f45712db89ab194bd3bf4d
Tom Sisk <tom.sisk@barchart.com>
2019-10-14 23:33:29 +0000

    remove duplicate method definitions

--- src/Contracts/Authenticatable.php
+++ src/Contracts/Authenticatable.php
@@ -7,27 +7,6 @@ use Illuminate\Contracts\Auth\Authenticatable as BaseAuthenticatable;
 interface Authenticatable extends BaseAuthenticatable
 {
     /**
-     * Get the name of the unique identifier for the user.
-     *
-     * @return string
-     */
-    public function getAuthIdentifierName();
-
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
-    public function getAuthIdentifier();
-
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
-    public function getAuthPassword();
-
-    /**
      * Get the "remember me" session tokens for the user.
      *
      * @return string
